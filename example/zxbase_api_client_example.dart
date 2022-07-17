92845a1ad72b3df7a4c52855ab45a72718108fdf
Ariel Tubaltsev <ariel@zxbase.com>
2022-07-17 11:15:54 -0700

    initial package structure

---
+++ example/zxbase_api_client_example.dart
@@ -0,0 +1,6 @@
+import 'package:zxbase_api_client/zxbase_api_client.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/zxbase_api_client.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library zxbase_api_client;
+
+export 'src/zxbase_api_client_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
