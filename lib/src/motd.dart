a73a4d246fbb72ab034dc5153c9f349878dba265
Ariel Tubaltsev <tubaltzev@gmail.com>
2024-04-04 21:40:35 -0700

    fix library comments

--- lib/src/motd.dart
+++ lib/src/motd.dart
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/// Message of the day.
+// Message of the day.
 
 class MOTD {
   MOTD(
--- lib/src/rps_client.dart
+++ lib/src/rps_client.dart
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/// RPS (Registration and pairing service) client.
+// RPS (Registration and pairing service) client.
 
 import 'dart:convert';
 import 'dart:developer';
